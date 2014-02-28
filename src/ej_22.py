d16f388e5ec870bd8db2efe1ca4a7c5097db5f79
Cirilo Fleitas Rufino <alu0100612882@ull.edu.es>
2014-02-28 09:49:41 +0000

    Ejercicio 22

---
+++ src/ej_22.py
@@ -0,0 +1,8 @@
+#!usr/bin/python
+
+a = float(raw_input('Valor de a:'))
+b = float(raw_input('Valor de b:'))
+if a != 0 :
+  x = -b/a
+  print 'solucion ',x
+  print 'si a=0 y b=0 entonces la ecuacion tiene infinitas soluciones' 
