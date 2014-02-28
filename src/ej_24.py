a3c88e25d4a619912377d48dbf51767843fea032
Cirilo Fleitas Rufino <alu0100612882@ull.edu.es>
2014-02-28 10:03:46 +0000

    Ejercicio 24

--- src/ej_24.py
+++ src/ej_24.py
@@ -7,3 +7,12 @@ if a != 0 :
   x1 = (-b + sqrt(b**2-4*a*c))/(2*a)
   x2 = (-b - sqrt(b**2-4*a*c))/(2*a)
   print 'las soluciones de la ecuacion son: x1=%4.3f y x2=%4.3f' % (x1,x2)
+else:
+  if b != 0:
+    x = -c/b
+    print 'La solucion de la ecuacion es: x=%4.3f' % x
+  else:
+    if c != 0:
+      print 'La ecuacion no tiene solucion'
+    else:
+      print 'La ecuacion tiene infinitas soluciones'
